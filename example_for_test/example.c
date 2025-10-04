f2f5566da9044eb4a2130e5cde64d36ff4832242
Falk Bruskeland <falk.bruskeland@gmail.com>
2025-10-04 19:20:06 +0200

    style: change clang-format style to Microsoft

--- example_for_test/example.c
+++ example_for_test/example.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float multiply_win(float a, float b) { return a * b; }
+float multiply_win(float a, float b)
+{
+    return a * b;
+}
 
-char *return_c() { return "hei!"; }
+char *return_c()
+{
+    return "hei!";
+}
