6c939e3a875d243a1a32cca64c48de1f783f2a70
Falk Bruskeland <falk.bruskeland@gmail.com>
2025-10-29 21:25:59 +0100

    feat: update game

--- platform/desktop/src/main.c
+++ platform/desktop/src/main.c
@@ -5,6 +5,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -13,9 +15,6 @@ static SDL_Renderer *renderer = NULL;
 #define WINDOW_HEIGHT 272
 
 #include "framebuffer.h"
-#include "primitives/image.h"
-#include "primitives/text.h"
-#include "primitives/rectangle.h"
 #include "widgets/button.h"
 #include "widgets/label.h"
 #include "widgets/canvas.h"
@@ -26,63 +25,114 @@ Framebuffer framebuffer;
 
 #define FRAMEBUFFER_SIZE WINDOW_WIDTH *WINDOW_HEIGHT
 
-static Widget *button1 = NULL;
-static Widget *button2 = NULL;
+static const char *DRAWING_PROMPTS[] = {
+    "Cat",
+    "House",
+    "Tree",
+    "Car",
+    "Sun",
+    "Flower",
+    "Star",
+    "Fish",
+    "Heart",
+    "Circle"
+};
+#define NUM_PROMPTS 10
+
+typedef enum {
+    GAME_STATE_PLAYING,
+    GAME_STATE_RESULT
+} GameState;
+
+static Widget *label_prompt = NULL;
+static Widget *label_result = NULL;
+static Widget *button_guess = NULL;
 static Widget *button_clear = NULL;
-static Widget *label_title = NULL;
-static Widget *label_counter = NULL;
+static Widget *button_retry = NULL;
 static Widget *canvas = NULL;
 
-static int click_count = 0;
+static GameState game_state = GAME_STATE_PLAYING;
+static int current_target_index = 0;
 static bool needs_redraw = true;
 static bool is_drawing = false;
 
-void on_button1_click(Widget *widget, void *user_data)
+void start_new_round()
 {
-    click_count++;
-    printf("Button 1 clicked! Total clicks: %d\n", click_count);
+    current_target_index = rand() % NUM_PROMPTS;
+    game_state = GAME_STATE_PLAYING;
+
+    char prompt_text[128];
+    snprintf(prompt_text, sizeof(prompt_text), "Draw a: %s", DRAWING_PROMPTS[current_target_index]);
+    label_set_text(label_prompt, prompt_text);
+    label_auto_size(label_prompt);
+
+    label_set_text(label_result, "");
+
+    widget_set_visible(button_guess, true);
+    widget_set_visible(button_clear, true);
+    widget_set_visible(button_retry, false);
+
+    canvas_clear(canvas);
     needs_redraw = true;
 }
 
-void on_button2_click(Widget *widget, void *user_data)
+void on_guess_click(Widget *widget, void *user_data)
 {
-    printf("Button 2 clicked! Resetting counter.\n");
-    click_count = 0;
+    int guess_index = rand() % NUM_PROMPTS;
+    const char *guess = DRAWING_PROMPTS[guess_index];
+
+    bool correct = (guess_index == current_target_index);
+
+    char result_text[256];
+    if (correct) {
+        snprintf(result_text, sizeof(result_text), "Correct! I guessed: %s", guess);
+    } else {
+        snprintf(result_text, sizeof(result_text), "Incorrect! I guessed: %s",
+                 guess, DRAWING_PROMPTS[current_target_index]);
+    }
+
+    label_set_text(label_result, result_text);
+    label_auto_size(label_result);
+
+    game_state = GAME_STATE_RESULT;
+    widget_set_visible(button_guess, false);
+    widget_set_visible(button_clear, false);
+    widget_set_visible(button_retry, true);
+
     needs_redraw = true;
 }
 
 void on_clear_canvas_click(Widget *widget, void *user_data)
 {
-    printf("Clearing canvas.\n");
     canvas_clear(canvas);
     needs_redraw = true;
+}
+
+void on_retry_click(Widget *widget, void *user_data)
+{
+    start_new_round();
 }
 
 void render_framebuffer()
 {
     for (int i = 0; i < FRAMEBUFFER_SIZE; i++)
     {
-        framebuffer.pixels[i] = 0;
+        framebuffer.pixels[i] = 50;
     }
 
-    char counter_text[64];
-    snprintf(counter_text, sizeof(counter_text), "Clicks: %d", click_count);
-    label_set_text(label_counter, counter_text);
-    label_auto_size(label_counter);
-
-    widget_render(label_title, &framebuffer);
-    widget_render(label_counter, &framebuffer);
-    widget_render(button1, &framebuffer);
-    widget_render(button2, &framebuffer);
-    widget_render(button_clear, &framebuffer);
+    widget_render(label_prompt, &framebuffer);
     widget_render(canvas, &framebuffer);
-
-    renderImage(10, 180, &framebuffer);
+    widget_render(button_guess, &framebuffer);
+    widget_render(button_clear, &framebuffer);
+    widget_render(button_retry, &framebuffer);
+    widget_render(label_result, &framebuffer);
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    SDL_SetAppMetadata("GUI Library Example", "1.0", "com.example.renderer-points");
+    srand(time(NULL));
+
+    SDL_SetAppMetadata("Drawing Game", "1.0", "com.example.drawing-game");
 
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
@@ -90,7 +140,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("GUI Demo", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer))
+    if (!SDL_CreateWindowAndRenderer("Drawing Game", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -103,38 +153,38 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     framebuffer = (Framebuffer){pixels, WINDOW_WIDTH, WINDOW_HEIGHT};
 
-    label_title = label_create_auto(10, 10, "Widget Demo - Click the buttons!", &font_font);
-    label_set_color(label_title, COLOR_WHITE);
+    label_prompt = label_create_auto(10, 10, "Draw a: Cat", &font_font);
+    label_set_color(label_prompt, COLOR_WHITE);
 
-    label_counter = label_create_auto(10, 30, "Clicks: 0", &font_font);
-    label_set_color(label_counter, COLOR_WHITE);
+    canvas = canvas_create(10, 35, 460, 150);
+    canvas_set_brush_size(canvas, 3);
+    canvas_set_brush_color(canvas, COLOR_BLACK);
+    canvas_set_border(canvas, COLOR_WHITE, 2);
 
-    button1 = button_create_auto(10, 60, "Click Me!", &font_font);
-    button_set_background_color(button1, COLOR_GRAY_75);
-    button_set_text_color(button1, COLOR_BLACK);
-    button_set_border(button1, COLOR_WHITE, 2);
-    button_set_on_click(button1, on_button1_click, NULL);
+    button_guess = button_create_auto(10, 195, "Guess", &font_font);
+    button_set_background_color(button_guess, COLOR_GRAY_75);
+    button_set_text_color(button_guess, COLOR_BLACK);
+    button_set_border(button_guess, COLOR_WHITE, 2);
+    button_set_on_click(button_guess, on_guess_click, NULL);
 
-    int button2_x = button1->x + button1->width + 10;
-    button2 = button_create_auto(button2_x, 60, "Reset Count", &font_font);
-    button_set_background_color(button2, COLOR_GRAY_75);
-    button_set_text_color(button2, COLOR_BLACK);
-    button_set_border(button2, COLOR_WHITE, 2);
-    button_set_on_click(button2, on_button2_click, NULL);
-
-    int button3_x = button2->x + button2->width + 10;
-    button_clear = button_create_auto(button3_x, 60, "Clear Canvas", &font_font);
+    int clear_x = button_guess->x + button_guess->width + 10;
+    button_clear = button_create_auto(clear_x, 195, "Clear Canvas", &font_font);
     button_set_background_color(button_clear, COLOR_GRAY_75);
     button_set_text_color(button_clear, COLOR_BLACK);
     button_set_border(button_clear, COLOR_WHITE, 2);
     button_set_on_click(button_clear, on_clear_canvas_click, NULL);
 
-    canvas = canvas_create(10, 105, 280, 150);
-    canvas_set_brush_size(canvas, 5);
-    canvas_set_brush_color(canvas, COLOR_BLACK);
-    canvas_set_border(canvas, COLOR_WHITE, 2);
+    button_retry = button_create_auto(clear_x, 195, "Try Again", &font_font);
+    button_set_background_color(button_retry, COLOR_GRAY_75);
+    button_set_text_color(button_retry, COLOR_BLACK);
+    button_set_border(button_retry, COLOR_WHITE, 2);
+    button_set_on_click(button_retry, on_retry_click, NULL);
+    widget_set_visible(button_retry, false);
 
-    render_framebuffer();
+    label_result = label_create_auto(10, 250, "", &font_font);
+    label_set_color(label_result, COLOR_WHITE);
+
+    start_new_round();
 
     return SDL_APP_CONTINUE;
 }
@@ -153,7 +203,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
         if (event->button.button == SDL_BUTTON_LEFT)
         {
-            if (widget_contains_point(canvas, mouse_x, mouse_y))
+            if (widget_contains_point(canvas, mouse_x, mouse_y) && game_state == GAME_STATE_PLAYING)
             {
                 is_drawing = true;
                 canvas_draw_at(canvas, mouse_x, mouse_y);
@@ -161,9 +211,9 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
             }
             else
             {
-                widget_handle_click(button1, mouse_x, mouse_y);
-                widget_handle_click(button2, mouse_x, mouse_y);
+                widget_handle_click(button_guess, mouse_x, mouse_y);
                 widget_handle_click(button_clear, mouse_x, mouse_y);
+                widget_handle_click(button_retry, mouse_x, mouse_y);
             }
         }
     }
@@ -178,7 +228,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
     if (event->type == SDL_EVENT_MOUSE_MOTION)
     {
-        if (is_drawing)
+        if (is_drawing && game_state == GAME_STATE_PLAYING)
         {
             int mouse_x = (int)event->motion.x;
             int mouse_y = (int)event->motion.y;
@@ -218,30 +268,30 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    if (label_title)
+    if (label_prompt)
     {
-        widget_destroy(label_title);
-        free(label_title);
+        widget_destroy(label_prompt);
+        free(label_prompt);
     }
-    if (label_counter)
+    if (label_result)
     {
-        widget_destroy(label_counter);
-        free(label_counter);
+        widget_destroy(label_result);
+        free(label_result);
     }
-    if (button1)
+    if (button_guess)
     {
-        widget_destroy(button1);
-        free(button1);
-    }
-    if (button2)
-    {
-        widget_destroy(button2);
-        free(button2);
+        widget_destroy(button_guess);
+        free(button_guess);
     }
     if (button_clear)
     {
         widget_destroy(button_clear);
         free(button_clear);
+    }
+    if (button_retry)
+    {
+        widget_destroy(button_retry);
+        free(button_retry);
     }
     if (canvas)
     {
@@ -253,6 +303,4 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
     {
         free(framebuffer.pixels);
     }
-
-    printf("Application closed. Total clicks: %d\n", click_count);
 }
